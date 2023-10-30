392d9a702f5c229dadb31cdffbfb77522bc7c26c
TheFred94 <frederikrlarsen@gmail.com>
2023-10-30 23:04:03 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  // Your other Next.js configuration settings
+  experiments: {
+    clientComponents: true,
+  },
+};
---
+++ pages/api/twitchAuth.js
@@ -0,0 +1,28 @@
+// twitchAuth.js
+
+import axios from "axios";
+
+const CLIENT_ID = process.env.NEXT_PUBLIC_TWITCH_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_TWITCH_SECRET;
+
+const getToken = async () => {
+  try {
+    const response = await axios.post(
+      `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`,
+      null, // Use null as the data
+      {
+        params: {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          grant_type: "client_credentials",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to get Twitch access token");
+  }
+};
+
+export default getToken;
