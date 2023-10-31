f09cbf4c374828814365103f6b7144c46390f0fc
TheFred94 <frederikrlarsen@gmail.com>
2023-10-31 14:24:19 +0100

    The api works

--- pages/api/igdb.js
+++ pages/api/igdb.js
@@ -1,5 +1,5 @@
 import getToken from "./twitchAuth";
-import axios from "axios";
+
 const handler = async (req, res) => {
   try {
     const tokenData = await getToken();
@@ -12,21 +12,24 @@ const handler = async (req, res) => {
     const accessToken = tokenData.access_token;
     console.log(accessToken);
 
-    // Now, you have the access token, and you can use it to make a request to the IGDB API.
-    const igdbResponse = await axios.post(
-      "https://api.igdb.com/v4/games",
-      null, // Use null as the data
-      {
-        headers: {
-          "Client-ID": process.env.NEXT_PUBLIC_TWITCH_ID,
-          Authorization: `Bearer ${accessToken}`,
-          Body: "fields *;",
-        },
-      }
-    );
+    // Now, you have the access token, and you can use it to make a request to the IGDB API using the fetch function.
+    const igdbResponse = await fetch("https://api.igdb.com/v4/games", {
+      method: "POST",
+      headers: {
+        "Client-ID": process.env.NEXT_PUBLIC_TWITCH_ID,
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
+    });
 
-    // Handle the IGDB API response as needed and send it back to the client.
-    res.status(200).json(igdbResponse.data);
+    if (!igdbResponse.ok) {
+      throw new Error("Failed to fetch data from IGDB API");
+    }
+
+    // Parse the response as JSON and send it back to the client.
+    const igdbData = await igdbResponse.json();
+    res.status(200).json(igdbData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
